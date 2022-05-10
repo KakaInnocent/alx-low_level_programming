dec7a48120d4063a25c3a08e53ad795623153d28
Mambo Innocent <saneugene847@gmail.com>
2022-05-10 08:57:53 -0700

    trial

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,9 +2,13 @@
 #define _MAIN_H_
 
 void *malloc_checked(unsigned int b);
+int _checknum(char *argv[], int n);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
+char *_initialize_array(char *ar, int lar);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void _is_zero(char *argv[]);
+int _strlen(char *s);
 
 #endif
