fecb2377c31df04c5133bf7e64cbddb3ca66a55b
Mambo Innocent <saneugene847@gmail.com>
2022-05-16 15:00:18 -0700

    task 2 fixed

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,4 @@
-#include "function_pointers.c"
+#include "function_pointers.h"
 
 /**
  *int_index - searches for an integer.
@@ -7,7 +7,7 @@
  *@cmp: pointer to the comparing function.
  *
  *Return: index of the first element for which
- *the cmo function does not return 0, or -1 if no match is found.
+ *the cmp function does not return 0, or -1 if no match is found.
  *or size is negative.
  */
 int int_index(int *array, int size, int (*cmp)(int))
