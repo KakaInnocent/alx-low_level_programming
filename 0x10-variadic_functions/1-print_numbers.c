debbf1d03c3c57d0ee277d3e371df937542d8ad9
Mambo Innocent <saneugene847@gmail.com>
2022-05-17 05:08:30 -0700

    int printf

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,7 +16,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		if (!separator)
 			printf("%d", va_arg(verlist, int));
-		else if (separator && i == 0)
+		else if (separator && j == 0)
 			printf("%d", va_arg(verlist, int));
 		else
 			printf("%s%d", separator, va_arg(verlist, int));
