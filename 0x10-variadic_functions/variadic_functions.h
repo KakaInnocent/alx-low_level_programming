68cbc4cdb5c151e5bcbe2d3d783ff0e42df5d7c0
Mambo Innocent <saneugene847@gmail.com>
2022-05-17 02:56:46 -0700

    first commit

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef VAR_FUNCTIONS
+#define VAR_FUNCTIONS
+
+#include <stdarg.h>
+#include <stdio.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
