75cde4641e9132b8023aad48cd25c1002ef87ec6
Mambo Innocent <saneugene847@gmail.com>
2022-05-18 17:19:20 -0700

    fixes

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -28,13 +28,13 @@ void print_all(const char * const format, ...)
 		switch (format[i])
 		{
 			case 'c':
-				printf("%s%c", va_arg(verlist, int)), c = 1;
+				printf("%c", va_arg(verlist, int)), c = 1;
 				break;
 			case 'i':
-				printf("%s%d", va_arg(verlist, int)), c = 1;
+				printf("%d", va_arg(verlist, int)), c = 1;
 				break;
 			case 'f':
-				printf("%s%f", va_arg(verlist, double)), c = 1;
+				printf("%f", va_arg(verlist, double)), c = 1;
 				break;
 			case 's':
 				str = va_arg(verlist, char *), c = 1;
