c0c41c40cb7330eaeca7c6f31d8ab6c66ed2a356
Mambo Innocent <saneugene847@gmail.com>
2022-06-02 16:07:39 -0700

    whoa

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,70 +1,50 @@
 #include "main.h"
 
 /**
- *err_file - checks whether a file is accessible
- *@file_frm: source of the file
- *@file_t: destination of the file
- *@argv: arguments vector
- *Return: no return
+ *main - copies the content of a file to another file
+ *@argc: number of arguments passed to the program
+ *@argv: array of arguments
+ *Return: Always 0
  */
-void err_file(int file_frm, int file_t, char *argv[])
+int main(int argc, char *argv[])
 {
-	if (file_frm == -1)
+	int dfr, dfw, k, l, m;
+	char buf[BUFSIZ];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp fil_frm fil_t\n");
+		exit(97);
+	}
+	dfr = open(argv[1], O_RDONLY);
+	if (dfr < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (file_t == -1)
+	dfw = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((r = read(dfr, buf, BUFSIZ)) > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+		if (dfw < 0 || write(dfw, buf, k) != k)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(dfr);
+			exit(99);
+		}
 	}
-}
-
-/**
- *main -checks the code
- *@argc: number of arguments
- *@argv: arguments vector
- *Return: always 0
- */
-int main(int argc, char *argv[])
-{
-	int file_frm, file_t err_exit;
-	ssize_t m, k;
-	char buf[1024];
-
-	if (argc != 3)
+	if (k < 0)
 	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_frm file_t\n");
-		exit(97);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
-
-	file_frm = open(argv[1], O_RDONLY);
-	file_t = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	err_file(file_frm, file_t, argv);
-
-	m = 1024;
-	while (m == 1024)
+	l = close(dfr);
+	m = close(dfw);
+	if (l < 0 || m < 0)
 	{
-		m = read(file_frm, buf, 1024);
-		if (m == -1)
-			err_file(-1, 0, argv);
-		k = write(file_t, buf, m);
-		if (k == -1)
-			err_file(0, -1, argv);
-	}
-
-	err_exit = close(file_frm);
-	if (err_exit == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_frm);
-		exit(100);
-	}
-
-	err_exit = close(file_t);
-	if (err_exit == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_frm);
+		if (l < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close df %d\n", dfr);
+		if (m < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close df %d\n", dfw);
 		exit(100);
 	}
 	return (0);
