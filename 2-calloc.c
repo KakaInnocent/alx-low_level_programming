03b489649d4f2dcba49ab74d95a554f522abd7ba
Mambo Innocent <saneugene847@gmail.com>
2022-07-29 03:34:05 -0700

    initial commit 1

---
+++ 2-calloc.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *_calloc - allocates a memory for an array.
+ *@nmemb: number of elements.
+ *@size: size of bytes
+ *
+ *Return: pointer to the allocated memory.
+ *if nmemb or size is 0, returns NULL.
+ *if malloc fails, returns NULL.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	unsigned int i;
+
+	if ()
